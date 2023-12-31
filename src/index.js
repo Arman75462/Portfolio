6e20fe21e765c9458e1df4fefe6c580954280a28
Arman <armangulistani@gmail.com>
2023-12-31 11:30:20 -0500

    Completed basic version of hero section

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 
 const hamburgerMenuButton = document.querySelector(".hamburger-wrapper");
 const navbarLinks = document.querySelector(".nav__list");
-const body = document.querySelector("body");
+const bodyElement = document.querySelector("body");
 
 hamburgerMenuButton.addEventListener("click", () => {
   hamburgerMenuButton.classList.toggle("active");
@@ -10,29 +10,10 @@ hamburgerMenuButton.addEventListener("click", () => {
   if (navbarLinks.classList.contains("show")) {
     navbarLinks.classList.remove("show");
     navbarLinks.classList.add("hide");
-    body.style.overflow = "auto"; // Set overflow back to 'auto'
+    bodyElement.style.overflow = "auto"; // Set overflow back to 'auto'
   } else {
     navbarLinks.classList.remove("hide");
     navbarLinks.classList.add("show");
-    body.style.overflow = "hidden"; // Set overflow to 'hidden'
+    bodyElement.style.overflow = "hidden"; // Set overflow to 'hidden'
   }
 });
-
-/* // This makes hamburger menu work
-let menuButton = document.querySelector(".menu-button");
-let navbarLinks = document.querySelector(".navbar-links");
-const body = document.querySelector("body");
-
-menuButton.addEventListener("click", function () {
-  if (navbarLinks.classList.contains("show")) {
-    navbarLinks.classList.remove("show");
-    navbarLinks.classList.add("hide");
-    menuButton.textContent = "≡";
-    body.style.overflow = "auto"; // Set overflow back to 'auto'
-  } else {
-    navbarLinks.classList.remove("hide");
-    navbarLinks.classList.add("show");
-    menuButton.textContent = "✖";
-    body.style.overflow = "hidden"; // Set overflow to 'hidden'
-  }
-}); */
