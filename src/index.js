6210edd4f0498989f26e618185bd4e5ac0220ad1
Arman <armangulistani@gmail.com>
2023-12-31 18:52:33 -0500

    Created custom cursor
    Created custom cursor to make the experience more enjoyable for the website visitor.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import "./index.css";
 
+/* SELECTORS */
 const hamburgerMenuButton = document.querySelector(".hamburger-wrapper");
 const navbarLinks = document.querySelector(".nav__list");
 const bodyElement = document.querySelector("body");
+const cursorDot = document.querySelector(".cursor-dot");
+const cursorOutline = document.querySelector(".cursor-outline");
 
+/* Event listener for customer cursor */
+window.addEventListener("mousemove", (e) => {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 500, fill: "forwards" }
+  );
+});
+
+// Event listener for hamburger menu
 hamburgerMenuButton.addEventListener("click", () => {
   hamburgerMenuButton.classList.toggle("active");
 
