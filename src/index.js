01ecbca460c774995bdd0abbb13c6be491719841
Arman <armangulistani@gmail.com>
2024-01-02 18:09:47 -0500

    Implemented menu auto-close on link click.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 
 /* SELECTORS */
 const hamburgerMenuButton = document.querySelector(".hamburger-wrapper");
-const navbarLinks = document.querySelector(".nav__list");
+const navbarLinksContainer = document.querySelector(".nav__list");
 const bodyElement = document.querySelector("body");
 const cursorDot = document.querySelector(".cursor-dot");
 const cursorOutline = document.querySelector(".cursor-outline");
@@ -24,17 +24,35 @@ window.addEventListener("mousemove", (e) => {
   );
 });
 
+// Function to hide menu
+function hideMenu() {
+  navbarLinksContainer.classList.remove("show");
+  navbarLinksContainer.classList.add("hide");
+  bodyElement.style.overflow = "auto"; // Set overflow back to 'auto'
+}
+
+// Function to show menu
+function showMenu() {
+  navbarLinksContainer.classList.remove("hide");
+  navbarLinksContainer.classList.add("show");
+  bodyElement.style.overflow = "hidden"; // Set overflow to 'hidden'
+}
+
+// Event listener to hide menu once user clicked on a link
+navbarLinksContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("nav__link")) {
+    hamburgerMenuButton.classList.toggle("active");
+    hideMenu();
+  }
+});
+
 // Event listener for hamburger menu
 hamburgerMenuButton.addEventListener("click", () => {
   hamburgerMenuButton.classList.toggle("active");
 
-  if (navbarLinks.classList.contains("show")) {
-    navbarLinks.classList.remove("show");
-    navbarLinks.classList.add("hide");
-    bodyElement.style.overflow = "auto"; // Set overflow back to 'auto'
+  if (navbarLinksContainer.classList.contains("show")) {
+    hideMenu();
   } else {
-    navbarLinks.classList.remove("hide");
-    navbarLinks.classList.add("show");
-    bodyElement.style.overflow = "hidden"; // Set overflow to 'hidden'
+    showMenu();
   }
 });
