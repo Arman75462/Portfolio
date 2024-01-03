9bc005e0fe20eac86a0d828396febe9ca056df7b
Arman <armangulistani@gmail.com>
2024-01-03 13:50:35 -0500

    Added lines on top of section titles
    Added border-top for each mai section title, to add aesthetics and a better UX.

--- src/javascript-files/selectors.js
+++ src/javascript-files/selectors.js
@@ -1,4 +1,4 @@
-/* SELECTORS */
+/* DOM SELECTORS */
 export const hamburgerMenuButton = document.querySelector(".hamburger-wrapper");
 export const navbarLinksContainer = document.querySelector(".nav__list");
 export const bodyElement = document.querySelector("body");
@@ -6,3 +6,6 @@ export const cursorDot = document.querySelector(".cursor-dot");
 export const cursorOutline = document.querySelector(".cursor-outline");
 export const languageSwitch = document.querySelector(".nav__language");
 export const resumeLink = document.querySelector(".about-me__link");
+
+// Select all elements with a 'data-lang-en' attribute
+export const translatableElements = document.querySelectorAll("[data-lang-en]");
