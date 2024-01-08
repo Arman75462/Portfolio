16f207be2fc705bb0b736e188df6c95b3fb99ab5
Arman <armangulistani@gmail.com>
2024-01-08 13:44:14 -0500

    Modified animations effects
    Modified hero animation effect. Now, instead of zooming-into each individual element, I packed the animation of the title and contact button together, to avoid code pollution. Also, that fixed the hover/focus state of the contact button not working. Plus, I added an offset of 350px in the initialization of each animations, to make the animations appear whelement are at 250px of the bottom of the screen.

--- src/javascript-files/index.js
+++ src/javascript-files/index.js
@@ -143,5 +143,6 @@ document.addEventListener("DOMContentLoaded", () => {
   AOS.init({
     duration: 1500, // values from 0 to 3000, with step 50ms
     once: true, // whether animation should happen only once - while scrolling down
+    offset: 350,
   });
 });
