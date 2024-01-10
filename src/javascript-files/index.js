aac6427ec1d508e422904b6d98ce528d11911bbc
Arman <armangulistani@gmail.com>
2024-01-10 18:16:38 -0500

    Modified AOS animations' offset
    Modified the animations' offset from 350px to 320px, because it takes too long for the images to appear at 350px, compared to 320px.

--- src/javascript-files/index.js
+++ src/javascript-files/index.js
@@ -158,7 +158,7 @@ document.addEventListener("DOMContentLoaded", () => {
     AOS.init({
       duration: 1500, // values from 0 to 3000, with step 50ms
       once: true, // whether animation should happen only once - while scrolling down
-      offset: 350,
+      offset: 320,
     });
   }
 });
