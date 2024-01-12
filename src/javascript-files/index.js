2f36e317c5be2ee56aa1165ce69449d1ac657ea3
Arman <armangulistani@gmail.com>
2024-01-12 10:29:03 -0500

    Changed condition for cursor to change position
    Only if window is larger or equal to 700px, will the position of the cursor change. This changed occur, for performance issues. Each time a user clicked on a screen, the position of the cursor would change, dropping the performance fo the website.

--- src/javascript-files/index.js
+++ src/javascript-files/index.js
@@ -27,19 +27,22 @@ window.addEventListener("scroll", updateScrollWatcher);
 
 // Event listener to make the custom cursor work
 window.addEventListener("mousemove", (e) => {
-  const posX = e.clientX;
-  const posY = e.clientY;
+  // Only if window is larger or equal 700px, do the following.
+  if (window.innerWidth >= 700) {
+    const posX = e.clientX;
+    const posY = e.clientY;
 
-  cursorDot.style.left = `${posX}px`;
-  cursorDot.style.top = `${posY}px`;
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
 
-  cursorOutline.animate(
-    {
-      left: `${posX}px`,
-      top: `${posY}px`,
-    },
-    { duration: 300, fill: "forwards" }
-  );
+    cursorOutline.animate(
+      {
+        left: `${posX}px`,
+        top: `${posY}px`,
+      },
+      { duration: 300, fill: "forwards" }
+    );
+  }
 });
 
 // Event listener to hide menu once user clicked on a link
